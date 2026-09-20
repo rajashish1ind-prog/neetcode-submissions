251b734bfe8210da98ab39a343d3ce53a5f39130
Ashish <rajashish1ind@gmail.com>
2026-09-20 18:11:03 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        for(char c : s) {
+
+            // Opening bracket
+            if(c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+
+            // Closing bracket
+            else {
+
+                if(st.empty())
+                    return false;
+
+                if(c == ')' && st.top() != '(')
+                    return false;
+
+                if(c == '}' && st.top() != '{')
+                    return false;
+
+                if(c == ']' && st.top() != '[')
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
